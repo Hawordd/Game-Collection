6d8dbf68c82477df71c7cedc8c55a3fa8fe1b7e8
Matthis <matthisgastesoyer@gmail.com>
2024-12-16 17:45:28 +0100

    Update paths

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     $request = strtok($request, '?');
 
     $routes = [
-        '/' => './Views/home.php',
-        '/index' => './Views/home.php',
-        '/home' => './Views/home.php',
-        '/librairy' => './Views/librairy.php',
-        '/profil' => './Views/profil.php',
-        '/scoreboard' => './Views/scoreboard.php',
-        '/edit' => './Views/edit.php',
-        '/add' => './Views/add.php',
-        '/login' => './Views/login.php',
-        '/register' => './Views/register.php',
-        '/404' => './Views/404.html'
+        '/' => 'Views/home.php',
+        '/index' => 'Views/home.php',
+        '/home' => 'Views/home.php',
+        '/librairy' => 'Views/librairy.php',
+        '/profil' => 'Views/profil.php',
+        '/scoreboard' => 'Views/scoreboard.php',
+        '/edit' => 'Views/edit.php',
+        '/add' => 'Views/add.php',
+        '/login' => 'Views/login.php',
+        '/register' => 'Views/register.php',
+        '/404' => 'Views/404.html'
     ];
 
     if (array_key_exists($request, $routes)) {
