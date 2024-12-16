da2a1fda36c83439617188298ae60d9699333f4d
Gael <gaelquem44@gmail.com>
2024-12-16 15:48:04 +0100

    correct index

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     $db = new PDO('mysql:host=localhost;dbname=................;charset=utf8', 'root', '');
 
-    require_once 'models/Game.php';
+    require_once 'Models/Game.php';
     $gameModel = new Game($db);
-    require_once 'models/User.php';
+    require_once 'Models/User.php';
     $userModel = new User($db);
 
-    require_once 'controllers/GameController.php';
+    require_once 'Controllers/GameController.php';
     $articleController = new ArticleController($articleModel);
 
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
