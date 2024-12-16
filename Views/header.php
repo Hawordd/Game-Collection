227e88d5f3f7def0bf5b7063fe1d841820c0eb86
Gael <gaelquem44@gmail.com>
2024-12-16 16:59:10 +0100

    commcé les view pour le header et footer

---
+++ Views/header.php
@@ -0,0 +1,9 @@
+    <header>
+        <a href=""><img src="../Assets/Images/logo.png" alt="Logo"></a>
+        <ul>
+            <li><a href="">Ma bibliothèque</a></li>
+            <li><a href="">Ajouter un jeu</a></li>
+            <li><a href="">Classement</a></li>
+            <li><a href="">Profil</a></li>
+        </ul>
+    </header>
