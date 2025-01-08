5fb7cc0004b901b457cf16093c905fb3231a3e51
Matthis <matthisgastesoyer@gmail.com>
2025-01-08 21:14:45 +0100

    Update db config to correctly find .env file

--- Config/DBConfig/config.php
+++ Config/DBConfig/config.php
@@ -4,7 +4,7 @@ namespace DBConfig;
 
 require 'vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 class Config
@@ -13,17 +13,25 @@ class Config
 
     public function __construct()
     {
-        self::$settings = [
-            'host' => $_ENV['DB_HOST'],
-            'port' => $_ENV['DB_PORT'],
-            'dbname' => $_ENV['DB_NAME'],
-            'user' => $_ENV['DB_USER'],
-            'password' => $_ENV['DB_PASSWORD']
-        ];
+        self::initialize();
+    }
+
+    private static function initialize(): void
+    {
+        if (!isset(self::$settings)) {
+            self::$settings = [
+                'host' => $_ENV['DB_HOST'],
+                'port' => $_ENV['DB_PORT'],
+                'dbname' => $_ENV['DB_NAME'],
+                'user' => $_ENV['DB_USER'],
+                'password' => $_ENV['DB_PASSWORD']
+            ];
+        }
     }
 
     public static function get($key)
     {
+        self::initialize();
         return self::$settings[$key] ?? null;
     }
 }
