5d70dabdf67b0dd65ebcea6948628e9e8ea3548a
Matthis <matthisgastesoyer@gmail.com>
2025-01-08 21:56:11 +0100

    Renamed librairy to library

--- Views/library.php
+++ Views/library.php
@@ -7,15 +7,7 @@
     <title>Game Collection - Bibliothèque</title>
 </head>
 <body>
-    <header>
-        <a href=""><img src="../Assets/Images/logo.png" alt="Logo"></a>
-        <ul>
-            <li><a href="">Ma bibliothèque</a></li>
-            <li><a href="">Ajouter un jeu</a></li>
-            <li><a href="">Classement</a></li>
-            <li><a href="">Profil</a></li>
-        </ul>
-    </header>
+    <?php include 'header.php'; ?>
     <main>
         <div id="search-game">
             <h1>Ajouter un jeu à sa bibliothèque</h1>
