d2a4a71a24ebac6f2f206c7d5e682cb20df057d8
Matthis <matthisgastesoyer@gmail.com>
2025-01-08 21:55:45 +0100

    Update header to show connect or disconnect

--- Views/header.php
+++ Views/header.php
@@ -4,6 +4,10 @@
             <li><a href="">Ma bibliothèque</a></li>
             <li><a href="">Ajouter un jeu</a></li>
             <li><a href="">Classement</a></li>
-            <li><a href="">Profil</a></li>
+            <?php if (isset($_SESSION['email'])) : ?>
+                <li><a href="disconnect">Déconnexion</a></li>
+            <?php else : ?>
+                <li><a href="login">Connexion</a></li>
+            <?php endif; ?>
         </ul>
     </header>
