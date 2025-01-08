e72784e3732b0e9267aa1bc60bf0f69a6e57f71c
Matthis <matthisgastesoyer@gmail.com>
2025-01-08 22:01:00 +0100

    Update header to add correct links

--- Views/header.php
+++ Views/header.php
@@ -1,9 +1,9 @@
     <header>
         <a href=""><img src="../Assets/Images/logo.png" alt="Logo"></a>
         <ul>
-            <li><a href="">Ma bibliothèque</a></li>
-            <li><a href="">Ajouter un jeu</a></li>
-            <li><a href="">Classement</a></li>
+            <li><a href="library">Ma bibliothèque</a></li>
+            <li><a href="add">Ajouter un jeu</a></li>
+            <li><a href="scoreboard">Classement</a></li>
             <?php if (isset($_SESSION['email'])) : ?>
                 <li><a href="disconnect">Déconnexion</a></li>
             <?php else : ?>
