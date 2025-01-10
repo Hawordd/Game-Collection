9dcf03347fb8e46a89c15cb5fa24a9aa42bb9ca1
Haword <150085140+Hawordd@users.noreply.github.com>
2025-01-10 17:04:50 +0100

    Update header to check user id instead of email

--- Views/header.php
+++ Views/header.php
@@ -4,8 +4,8 @@
             <li><a href="library">Ma bibliothèque</a></li>
             <li><a href="add">Ajouter un jeu</a></li>
             <li><a href="scoreboard">Classement</a></li>
-            <?php if (isset($_SESSION['email'])) : ?>
-                <li><a href="disconnect">Déconnexion</a></li>
+            <?php if (isset($_SESSION['id'])) : ?>
+                <li><a href="profil">Profil</a></li>
             <?php else : ?>
                 <li><a href="login">Connexion</a></li>
             <?php endif; ?>
