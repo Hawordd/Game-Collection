fa2184ffc43fbd8e5f55f37a6778d10f41620f0b
Haword <150085140+Hawordd@users.noreply.github.com>
2025-01-10 17:11:18 +0100

    Update index - redirect to login page when not logged in

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <?php
-    include_once __DIR__ . '/init.php';
 
-    $request = $_SERVER['REQUEST_URI'];
+session_start();
 
-    $request = strtok($request, '?');
+$request = $_SERVER['REQUEST_URI'];
+$request = strtok($request, '?');
 
-    $routes = [
-        '/' => 'Views/home.php',
-        '/index' => 'Views/home.php',
-        '/home' => 'Views/home.php',
-        '/library' => 'Views/library.php',
-        '/profil' => 'Views/profil.php',
-        '/scoreboard' => 'Views/scoreboard.php',
-        '/edit' => 'Views/edit.php',
-        '/add' => 'Views/add.php',
-        '/login' => 'Views/login.php',
-        '/register' => 'Views/register.php',
-        '/disconnect' => 'Views/disconnect.php',
-        '/404' => 'Views/404.html'
-    ];
+if (!isset($_SESSION['id']) && $request !== '/login' && $request !== '/register') {
+    header('Location: /login');
+    exit();
+}
 
-    if (array_key_exists($request, $routes)) {
-        include $routes[$request];
-    } else {
-        http_response_code(404);
-        header('Location: /404');
-    }
+$routes = [
+    '/' => 'Views/home.php',
+    '/index' => 'Views/home.php',
+    '/home' => 'Views/home.php',
+    '/library' => 'Views/library.php',
+    '/profil' => 'Views/profil.php',
+    '/scoreboard' => 'Views/scoreboard.php',
+    '/edit' => 'Views/edit.php',
+    '/add' => 'Views/add.php',
+    '/login' => 'Views/login.php',
+    '/register' => 'Views/register.php',
+    '/disconnect' => 'Views/disconnect.php',
+    '/404' => 'Views/404.html'
+];
+
+if (array_key_exists($request, $routes)) {
+    include $routes[$request];
+} else {
+    http_response_code(404);
+    header('Location: /404');
+}
 ?>
