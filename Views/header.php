b87c991ae0651576061c432f7219d6c1d199161b
Haword <150085140+Hawordd@users.noreply.github.com>
2025-01-10 17:17:29 +0100

    Update header

--- Views/header.php
+++ Views/header.php
@@ -1,8 +1,8 @@
     <header>
         <a href="/"><img src="../Assets/Images/logo.png" alt="Logo"></a>
         <ul>
-            <li><a href="library">Ma bibliothèque</a></li>
-            <li><a href="add">Ajouter un jeu</a></li>
+            <li><a href="/">Ma bibliothèque</a></li>
+            <li><a href="library">Ajouter un jeu</a></li>
             <li><a href="scoreboard">Classement</a></li>
             <?php if (isset($_SESSION['id'])) : ?>
                 <li><a href="profil">Profil</a></li>
