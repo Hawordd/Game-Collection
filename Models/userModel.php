02e356699ee99b8e6d5a53cbd7caeb3e0cdab38f
Gael <gaelquem44@gmail.com>
2025-01-11 15:36:08 +0100

    correct request sql

--- Models/userModel.php
+++ Models/userModel.php
@@ -19,7 +19,7 @@ class UserModel {
     public function getUserScordboard() {
         $req = $this->db->prepare('SELECT u.nom_utili AS Joueur, SUM(t.temps_jeux) AS "Temps total passé", (SELECT j2.nom_jeux FROM TEMPS t2 JOIN JEUX j2 ON t2.id_jeux = j2.id_jeux WHERE t2.id_utili = u.id_utili ORDER BY t2.temps_jeux DESC LIMIT 1) AS "Jeu favori" FROM UTILISATEUR u JOIN TEMPS t ON u.id_utili = t.id_utili WHERE t.temps_jeux >= 0 GROUP BY u.id_utili ORDER BY u.nom_utili');
         $req->execute();
-        return $req->fetch();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getUser($id) {
