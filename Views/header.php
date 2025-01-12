c7e12dd4b3f42dcdbea5a78be1d418e0461cb770
Gael <gaelquem44@gmail.com>
2025-01-12 14:00:22 +0100

    correction header

--- Views/header.php
+++ Views/header.php
@@ -2,12 +2,12 @@
         <a href="/"><img src="../Assets/Images/logo.png" alt="Logo"></a>
         <ul>
             <li><a href="/">Ma bibliothèque</a></li>
-            <li><a href="library">Ajouter un jeu</a></li>
-            <li><a href="scoreboard">Classement</a></li>
+            <li><a href="/library">Ajouter un jeu</a></li>
+            <li><a href="/scoreboard">Classement</a></li>
             <?php if (isset($_SESSION['id'])) : ?>
-                <li><a href="profil">Profil</a></li>
+                <li><a href="/profil">Profil</a></li>
             <?php else : ?>
-                <li><a href="login">Connexion</a></li>
+                <li><a href="/login">Connexion</a></li>
             <?php endif; ?>
         </ul>
     </header>
